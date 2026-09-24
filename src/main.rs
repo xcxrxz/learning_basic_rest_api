d2f50bcc7f42805654cf9378a00b04b0a356bac7
x_X <110759417+xcxrxz@users.noreply.github.com>
2026-09-24 00:25:04 -0600

    feat: add hello world http server (#1)
    Agrega el primer servidor Axum con una ruta / de prueba.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(root));
+    
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    println!("Servidor escuchando en http://localhost:3000");
+
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, world!"
 }

e35119cd1c2a5746e3de669fbe8c0c019ee6f101
x_X <110759417+xcxrxz@users.noreply.github.com>
2026-09-25 00:57:30 -0600

    feat: add Task model with serde derive (#2)
    Se agrega el modelo de datos para las tareas, y se comprueba el funcionamiento de Serialize

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
-use axum::{Router, routing::get};
+use axum::{Json, Router, routing::get};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Task {
+    pub id: i64,
+    pub title: String,
+    pub description: Option<String>,
+    pub completed: bool,
+}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(root));
-    
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/tasks/demo", get(demo_task));
+
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
     println!("Servidor escuchando en http://localhost:3000");
@@ -13,4 +24,13 @@ async fn main() {
 
 async fn root() -> &'static str {
     "Hello, world!"
+}
+
+async fn demo_task() -> Json<Task> {
+    Json(Task {
+        id: 1,
+        title: "Aprender Rust".to_string(),
+        description: Some("Terminar el tutorial de API REST".to_string()),
+        completed: false,
+    })
 }
